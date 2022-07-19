173351732fe6ca03493c3887cd54bce319f5d364
Shiv Dutt Jha <109537339+meshivdutt@users.noreply.github.com>
2022-07-19 10:42:40 +0530

    Create evenodd.cpp

---
+++ evenodd.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int t;
+cin>>t;
+while(t--){
+int n;
+cin>>n;
+if(n&1==1)
+{
+  cout<<"Odd"<<endl;
+ }
+ else
+ {
+ cout<<"even"<<endl;
+ }
+ }
+ return 0;
+ }
+  
